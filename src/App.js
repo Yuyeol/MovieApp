c6490cc4d28915b15f199c47c716e12987f6d911
Yuyeol <uy23@naver.com>
2020-09-21 22:39:36 +0900

    JSX bf CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 
 // function component는 retunr으로 screen에 표시,
 // class component는 react component로부터 확장되고 screen에 표시됨
@@ -7,14 +10,48 @@ class App extends React.Component {
   state = {
     isLoading: true,
   };
+
+  getMovies = async () => {
+    //movie 파일 불러오는것
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+  };
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      //JSX에서는 class가 아니라 className이라고 써야함.
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 
