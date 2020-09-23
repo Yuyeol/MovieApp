c5d9db9f60e22d0a345d5f88065f7ab5cafc2588
Yuyeol <uy23@naver.com>
2020-09-23 20:56:36 +0900

    Updates

---
+++ precache-manifest.b19a6edb426452739814736008b87511.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad4255e74548739cfaf2d7a7d9fbb8ca",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "dec1ba1aae80b61682d3",
+    "url": "/movie_app/static/css/main.d9df8995.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dec1ba1aae80b61682d3",
+    "url": "/movie_app/static/js/main.b023f13a.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
